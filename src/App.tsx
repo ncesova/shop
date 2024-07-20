e54d8feec858874dcb6c5c6c3019c93a59828a02
Sova <108209320+NceSova@users.noreply.github.com>
2024-07-20 22:29:30 +0300

    feat: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello, world!</h1>;
-}
+import {Outlet} from 'react-router-dom';
+import {NavBar} from './components/NavBar/NavBar';
 
-export default App;
+export function App() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+}
---
+++ src/Router.tsx
@@ -0,0 +1,36 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {LandingPage} from './components/Landing/LandingPage.tsx';
+import {StorePage} from './components/Store/StorePage.tsx';
+import {ItemPage} from './components/Items/ItemPage.tsx';
+import {CheckoutPage} from './components/Checkout/CheckoutPage.tsx';
+import {App} from './App.tsx';
+import {ErrorPage} from './components/ErrorPage/ErrorPage.tsx';
+
+export function Router() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: '/',
+          element: <LandingPage />,
+        },
+        {
+          path: '/store',
+          element: <StorePage />,
+        },
+        {
+          path: '/store/:itemId',
+          element: <ItemPage />,
+        },
+        {
+          path: '/checkout',
+          element: <CheckoutPage />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
