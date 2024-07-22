a69d20057dc11406370ac4e2321d44d9f08da332
Sova <108209320+NceSova@users.noreply.github.com>
2024-07-22 18:55:15 +0300

    feat: change select component

--- src/components/Store/StorePage.tsx
+++ src/components/Store/StorePage.tsx
@@ -1,16 +1,22 @@
 import {useItems} from '../../App';
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
+import Select from 'react-select';
 import {StoreItem} from './StoreItem';
 import {Item, Items} from '../../data/types';
 import {useEffect, useReducer, useState} from 'react';
 
+type Option = {
+  value: string;
+  label: string;
+};
+
 export function StorePage(this: any) {
-  const options = ['Category', 'Name', 'Price'];
   const [, forceUpdate] = useReducer((x) => x + 1, 0);
-  const defaultOption = options[0];
   const [items, setItems] = useState<Items | null>(null);
-  const [sort, setSort] = useState('Category');
+
+  const [selectedOption, setSelectedOption] = useState<Option>({
+    value: 'category',
+    label: 'Category',
+  });
 
   useEffect(() => {
     console.log('API fetch');
@@ -20,11 +26,11 @@ export function StorePage(this: any) {
   }, []);
 
   function compareBy(thing: string, a: Item, b: Item): number {
-    if (thing === 'Category') {
-      return a.category.localeCompare(b.category);
-    } else if (thing === 'Name') {
+    if (thing === 'category') {
+      return b.category.localeCompare(a.category);
+    } else if (thing === 'title') {
       return a.title.localeCompare(b.title);
-    } else if (thing === 'Price') {
+    } else if (thing === 'price') {
       return a.price - b.price;
     } else {
       return 0;
@@ -35,23 +41,26 @@ export function StorePage(this: any) {
     console.log('sorted');
     const newItems = items;
     newItems?.sort((a: Item, b: Item) => {
-      return compareBy(sort, a, b);
+      return compareBy(selectedOption.value, a, b);
     });
     setItems(newItems);
     forceUpdate();
-  }, [sort]);
+  }, [selectedOption]);
+
+  const options = [
+    {value: 'category', label: 'Category'},
+    {value: 'title', label: 'Name'},
+    {value: 'price', label: 'Price'},
+  ];
 
   return (
-    <div className="w-full">
+    <div className="">
       <div className="flex w-full items-center gap-4 px-8 py-2">
         <span>Sort by</span>
-        <Dropdown
+        <Select
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
           options={options}
-          value={sort}
-          onChange={(cur) => {
-            setSort(cur.value);
-          }}
-          placeholder="Select an option"
         />
       </div>
       <div className="grid grid-cols-1 gap-4 px-8 md:grid-cols-2 xl:grid-cols-3">
