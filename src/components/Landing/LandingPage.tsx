4d1de6244df2363c8d2f299ab77767497cab52cc
Sova <108209320+NceSova@users.noreply.github.com>
2024-07-23 21:39:30 +0300

    feat: add landing page

--- src/components/Landing/LandingPage.tsx
+++ src/components/Landing/LandingPage.tsx
@@ -1,3 +1,18 @@
+import {Link} from 'react-router-dom';
+
 export function LandingPage() {
-  return <div className="flex w-full items-center justify-center">Landing</div>;
+  return (
+    <div className="mt-20 flex h-max w-full flex-col items-center justify-center gap-6 p-4">
+      <span className="font-heading text-3xl font-bold">Welcome to Store</span>
+      <span className="font-body text-xl text-slate-500">
+        Discover new products every week for very pleasant prices
+      </span>
+      <Link
+        to="/store"
+        className="rounded-2xl bg-slate-700 p-6 font-body font-extrabold tracking-widest text-white">
+        SHOP NOW
+      </Link>
+      <img src="../src/assets/images/Sneaker.jpeg" className="mt-24" />
+    </div>
+  );
 }
